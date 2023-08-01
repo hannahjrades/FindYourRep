2df1193c1877b3401d10be020537d04a853b889c
Hannah Rades <hannahisradest@gmail.com>
2023-08-01 11:38:24 -0400

    Initial Commit

---
+++ FindYourRep/FindYourRepApp.swift
@@ -0,0 +1,17 @@
+//
+//  FindYourRepApp.swift
+//  FindYourRep
+//
+//  Created by scholar on 8/1/23.
+//
+
+import SwiftUI
+
+@main
+struct FindYourRepApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
