8c7c42264730794e0b113f9b05dfd6591d539273
Vesna Adjei <vadjei420@gmail.com>
2023-08-03 10:16:10 -0400

    part 1 of survey

--- FindYourRep/SurveyView.swift
+++ FindYourRep/SurveyView.swift
@@ -6,19 +6,75 @@
 //
 
 import SwiftUI
-
 struct SurveyView: View {
+    @State var bowserCount = 0
+    @State var grantCount = 0
+    @State var hallCount = 0
+    
+    @State var issue1result = Bool()
+    @State var issue2result = Bool()
+    @State var issue3result = Bool()
+    @State var issue4result = Bool()
+    @State var issue5result = Bool()
+    @State var issue6result = Bool()
     
     var body: some View {
         
         NavigationStack {
             
             VStack{
-                
-                NavigationLink(destination: ResultsView()) {
-                    Text("Submit")
-                } // for results link
+                Text("Choose which issues are important to you!")
+                Toggle(isOn: $issue1result) {
+                    Text("Increased police funding")
+                }
+                .padding()
+                Toggle(isOn: $issue2result) {
+                    Text("Mental Health")
+                }
+                .padding()
+                Toggle(isOn: $issue3result) {
+                    Text("Youth Programs")
+                }
+                .padding()
+                Toggle(isOn: $issue4result) {
+                    Text("Reproductive Rights")
+                }
+                .padding()
+                Toggle(isOn: $issue5result) {
+                    Text("Gun Control")
+                }
+                .padding()
+                Toggle(isOn: $issue6result) {
+                    Text("Tax Cuts")
+                }
+                .padding()
+                Button("Calculate") {
+                    if issue1result == true {
+                        bowserCount += 1
+                        hallCount += 2
+                    }
+                    if issue2result == true { bowserCount += 1
+                        grantCount += 1
+                    }
+                    if issue3result == true { grantCount += 2
+                    }
+                    if issue4result == true { bowserCount += 1
+                    }
+                    if issue5result == true { bowserCount += 2
+                        grantCount += 1
+                    }
+                    if issue6result == true {
+                        hallCount += 1
+                    }
+                }//for calculate button
                 .buttonStyle(.borderedProminent)
+                if bowserCount > grantCount && (hallCount != 0){
+                    NavigationLink(destination: ResultsView()) {
+                        Text("Submit")
+                    } // for results link
+                    .buttonStyle(.borderedProminent)
+                }
+                
                 
             } // for vStack
             
